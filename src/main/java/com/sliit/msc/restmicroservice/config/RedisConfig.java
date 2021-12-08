f42e9b95b63bf86a0334ed1e9c7439331ffc6926
Ashan Jayasundara <email2ashan@gmail.com>
2021-12-08 10:01:55 +0530

    add remote redis connection

--- src/main/java/com/sliit/msc/restmicroservice/config/RedisConfig.java
+++ src/main/java/com/sliit/msc/restmicroservice/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -29,11 +30,20 @@ public class RedisConfig {
     @Value("${topic.publisher}")
     private String channelName;
 
+    @Value("${redis.password}")
+    private String redisPassword;
+
+    @Value("${deployment.type}")
+    private String deploymentType;
+
     @Bean
     public JedisConnectionFactory connectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
         configuration.setHostName(redisHost);
         configuration.setPort(redisPort);
+        if (deploymentType.equalsIgnoreCase("PROD")) {
+            configuration.setPassword(RedisPassword.of(redisPassword));
+        }
         return new JedisConnectionFactory(configuration);
     }
 
